8e1456bfa14c60e13eb23f294e5d31197db9dd08
luminorena <94684347+luminorena@users.noreply.github.com>
2024-05-01 19:19:33 +0300

    Server from lesson

--- src/main/java/ru/otus/luminorena/web/server/HttpServer.java
+++ src/main/java/ru/otus/luminorena/web/server/HttpServer.java
@@ -1,12 +1,14 @@
 package ru.otus.luminorena.web.server;
 
+import ru.otus.luminorena.web.server.application.Storage;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
     private int port;
-    private Dispatcher dispatcher;
+    private ru.otus.luminorena.web.server.Dispatcher dispatcher;
 
     public HttpServer(int port) {
         this.port = port;
@@ -15,16 +17,20 @@ public class HttpServer {
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            this.dispatcher = new Dispatcher();
+            this.dispatcher = new ru.otus.luminorena.web.server.Dispatcher();
             System.out.println("Диспетчер проинициализирован");
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.info(true);
-
-                dispatcher.execute(request, socket.getOutputStream());
+            Storage.init();
+            while (true) {
+                try (Socket socket = serverSocket.accept()) {
+                    byte[] buffer = new byte[8192];
+                    int n = socket.getInputStream().read(buffer);
+                    if (n > 0) {
+                        String rawRequest = new String(buffer, 0, n);
+                        HttpRequest request = new HttpRequest(rawRequest);
+                        request.info(true);
+                        dispatcher.execute(request, socket.getOutputStream());
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
